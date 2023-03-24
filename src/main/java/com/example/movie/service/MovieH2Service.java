6600d3cf9ec6e189d59acbdb8059d108e387f67a
umamaheswari <numamaheswari02@gmail.com>
2023-03-24 06:47:45 +0000

    solution

---
+++ src/main/java/com/example/movie/service/MovieH2Service.java
@@ -0,0 +1,65 @@
+package com.example.movie.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+import java.util.*;
+
+//import javax.validation.OverridesAttribute;
+
+import com.example.movie.model.Movie;
+import com.example.movie.repository.MovieRepository;
+import com.example.movie.model.MovieRowMapper;
+
+// Write your code here
+
+@Service
+public class MovieH2Service implements MovieRepository{
+    @Autowired
+    private JdbcTemplate db;
+
+    @Override
+    public ArrayList<Movie> getMovies(){
+        List<Movie> list = db.query("SELECT * FROM MOVIELIST",new MovieRowMapper());
+        ArrayList<Movie> movies = new ArrayList<>(list);
+        return movies;
+    }
+
+    @Override
+    public Movie getMovieById(int movieId){
+        try{
+            Movie m = db.queryForObject("SELECT * FROM MOVIELIST WHERE movieId = ?", new MovieRowMapper(), movieId);
+            return m;
+        }
+        catch(Exception e){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public Movie addMovie(Movie movie){
+        db.update("INSERT INTO MOVIELIST(movieName, leadActor) values(?,?)", movie.getMovieName(), movie.getLeadActor());
+        Movie m = db.queryForObject("SELECT * FROM MOVIELIST WHERE movieName = ? and leadActor = ?", new MovieRowMapper(), movie.getMovieName(), movie.getLeadActor());
+        return m;
+    }
+
+    @Override
+    public Movie updateMovie(Movie movie, int movieId){
+        if(movie.getMovieName() != null){
+            db.update("UPDATE MOVIELIST SET movieName = ? WHERE movieId = ?", movie.getMovieName(), movieId);
+        }
+        if(movie.getLeadActor() != null){
+            db.update("UPDATE MOVIELIST SET leadActor = ? WHERE movieId = ?", movie.getLeadActor(), movieId);
+        }
+        return getMovieById(movieId);
+    }
+
+    @Override
+    public void deleteMovie(int movieId){
+        db.update("DELETE FROM movielist WHERE movieId = ?",movieId);
+    }
+    
+
+    
+}
